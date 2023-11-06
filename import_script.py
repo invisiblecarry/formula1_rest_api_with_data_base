9622ec309f1824efa8af31e7f1c453ec542ca228
LinuxVirtualBox <faceitinvisiblecarry@gmail.com>
2023-11-06 20:07:19 +0200

    6 commit

---
+++ import_script.py
@@ -0,0 +1,61 @@
+from formula1.report import parse_log_files, load_abbreviations, build_report
+import argparse
+import os
+from database.common.models import *
+
+parser = argparse.ArgumentParser(description="Import data from files to database.")
+parser.add_argument('--db-path', type=str, help='Path to the database.', required=True)
+parser.add_argument('--data-folder', type=str, help='Path to the folder with files.', required=True)
+
+
+def get_report(path_to_data_folder) -> dict:
+    start_data = parse_log_files(path_to_data_folder, 'start.log')
+    end_data = parse_log_files(path_to_data_folder, "end.log")
+    abbreviations = load_abbreviations(path_to_data_folder, "abbreviations.txt")
+    report = build_report(start_data, end_data, abbreviations)
+    return report
+
+
+def import_data_from_files(db: SqliteDatabase, path: str) -> None:
+    """
+        Imports race data from files into the database.
+
+        Parameters:
+        - db: Database object with connect and create_tables methods.
+        - path: String path to directory with race data files.
+
+        This function initializes database connection, creates tables, and populates them with
+        data parsed by `get_report` from the provided directory. It checks for existing entries
+        of racers and race results to avoid duplicates before creating new records.
+
+        Note: `Racers` and `RaceResults` are pre-defined ORM models.
+    """
+    report = get_report(path)
+    if not report:
+        raise TypeError('Can`t get the report')
+    database_proxy.initialize(database)
+    database_proxy.connect()
+    database_proxy.create_tables([Racers, RaceResults])
+    with db.atomic():
+        for driver in report.values():
+            existing_racer = Racers.select().where(Racers.abbreviation == driver['abbreviation']).first()
+            if not existing_racer:
+                racer = Racers.create(abbreviation=driver['abbreviation'],
+                                      driver_name=driver['driver_name'],
+                                      team=driver['team'])
+            else:
+                racer = existing_racer
+
+            existing_race_result = RaceResults.select().where(RaceResults.racer_id == racer.id).first()
+            if not existing_race_result:
+                race_result = RaceResults.create(racer_id=racer.id,
+                                                 start_time=driver['start_time'],
+                                                 end_time=driver['end_time'],
+                                                 best_lap_time=driver['best_lap_time'])
+    database_proxy.close()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    database = SqliteDatabase(os.path.join(args.db_path))
+    import_data_from_files(db=database, path=args.data_folder)
---
+++ rest_api_formula1_report.py
@@ -0,0 +1,51 @@
+import os
+import json
+import xml.etree.ElementTree as ET
+from flask import Flask, request
+from flask_restful import Api, Resource
+from flasgger import Swagger
+from formula1.report import parse_log_files, load_abbreviations, build_report
+
+
+app = Flask(__name__)
+app.config['DATA_FOLDER'] = os.path.abspath(os.path.join('', 'data'))
+api = Api(app)
+swagger = Swagger(app)
+
+
+def get_report():
+    start_data = parse_log_files(app.config['DATA_FOLDER'], "start.log")
+    end_data = parse_log_files(app.config['DATA_FOLDER'], "end.log")
+    abbreviations = load_abbreviations(app.config['DATA_FOLDER'], "abbreviations.txt")
+    report = build_report(start_data, end_data, abbreviations)
+    return report
+
+
+class ReportResource(Resource):
+
+    def get(self, version):
+        report = get_report()
+        data_format = request.args.get('format', 'json')
+        if data_format == 'json':
+            response_data = json.dumps(report)
+            return response_data, 200
+        elif data_format == 'xml':
+            root = ET.Element("report")
+            for driver_id, info in report.items():
+                driver = ET.SubElement(root, "driver")
+                ET.SubElement(driver, "DriverName").text = info['Driver Name']
+                ET.SubElement(driver, "Team").text = info['Team']
+                ET.SubElement(driver, "StartTime").text = info['Start time']
+                ET.SubElement(driver, "EndTime").text = info['End time']
+                ET.SubElement(driver, "BestLapTime").text = info['Best Lap Time']
+                ET.SubElement(driver, "Abbreviation").text = info['Abbreviation']
+            xml_response = ET.tostring(root, encoding="utf-8").decode('utf-8')
+            return xml_response, 200
+        else:
+            return "Invalid format. Use 'json' or 'xml'.", 400
+
+
+api.add_resource(ReportResource, '/api/<string:version>/report/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
